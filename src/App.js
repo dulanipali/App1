cfdaa6c858d0f0530e6ebcfeccaaffe03272a90c
dulanipali <dulani.2020@gmail.com>
2024-12-02 03:45:54 -0600

    Initial Prototype

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import MapWithSearch from './components/MapView';
 import BusSchedule from './components/BusSchedule'; // Import your Bus Schedule component
 import DiningOptions from './components/DiningOptions'; // Import your Dining Options component
 import Faq from './components/Faq'; // Import your FAQ component
-import Resources from './components/Resources';
 import MapWithDirections from './components/Directions';
 import Services from './components/Services';
 
@@ -12,13 +11,12 @@ console.log('Current environment:', process.env.NODE_ENV);
 
 function App() {
   return (
-    <Router basename='/RaiderNav'>
+    <Router basename='/App1'>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/BusSchedule" element={<BusSchedule />} />
         <Route path="/dining" element={<DiningOptions />} />
         <Route path="/faq" element={<Faq />} />
-        <Route path="/resources" element={<Resources />} />
         <Route path="/map" element={<MapWithSearch />} />
         <Route path="/directions" element={<MapWithDirections />} />
         <Route path="/services" element={<Services />} />
